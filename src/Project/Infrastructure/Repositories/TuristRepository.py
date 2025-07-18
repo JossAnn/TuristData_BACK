73e99045b5161ddffc0f15dd70cf00f2fab1da72
paolis28 <shinge@outlook.es>
2025-07-18 00:16:01 -0600

    Tratando de componer error de transtracción al registrar e iniar sesión inmediatamente

--- src/Project/Infrastructure/Repositories/TuristRepository.py
+++ src/Project/Infrastructure/Repositories/TuristRepository.py
@@ -2,30 +2,24 @@ from src.Project.Domain.Ports.ITuristRepository import IturistRepository
 from src.Project.Infrastructure.Models.TuristModel import TuristModel
 from src.DataBases.MySQL import SessionLocal
 
-
 class TuristRepository(IturistRepository):
-    def __init__(self):
-        self.db = SessionLocal()
     def get_user_by_id(self, id_):
-        return (
-            self.db.query(TuristModel)
-            .filter_by(id_usuario=id_)
-            .first()
-        )
+        with SessionLocal() as db:
+            return db.query(TuristModel).filter_by(id_usuario=id_).first()
 
     def create(self, turista):
-        nuevo = TuristModel(            
-            nombre=turista["nombre"],
-            correo=turista["correo"],
-            password=turista["password"]
-        )
-        self.db.add(nuevo)
-        self.db.commit()
-        self.db.refresh(nuevo)
-        return nuevo
+        with SessionLocal() as db:
+            nuevo = TuristModel(            
+                nombre=turista["nombre"],
+                correo=turista["correo"],
+                password=turista["password"]
+            )
+            db.add(nuevo)
+            db.commit()
+            db.refresh(nuevo)
+            return nuevo
     
     def login_turist_correo_password(self, correo, password):
-        # Buscar al turista por correo
-        turist = self.db.query(TuristModel).filter_by(correo=correo).first()
-        return turist 
-    
+        with SessionLocal() as db:
+            turist = db.query(TuristModel).filter_by(correo=correo).first()
+            return turist
